4a623e5f5063c68824d4a8fe65d97da446ba709c
Chinmay <yaminikashyap9@gmail.com>
2026-01-10 12:55:57 +0530

    this is the experiment 1 completed

---
+++ experiment-1.1/src/components/Spa.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './Homes';
+import About from './Abouts';
+import Contact from './Contacts';
+
+export default function SinglePageApp() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
